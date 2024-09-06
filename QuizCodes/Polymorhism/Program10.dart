34e2a4d2a943b9c6516146399ea3335298d421d0
adityamore2004 <adityamore5343@gmail.com>
2024-09-06 04:40:21 -0700

    yes boi

---
+++ QuizCodes/Polymorhism/Program10.dart
@@ -0,0 +1,19 @@
+abstract class Parent {
+  int x;
+  static int y = 6;
+  Parent(this.x);
+
+  void printData();
+}
+
+class Child implements Parent {
+  int y = 7;
+  void printData() {
+    print("This is printData");
+  }
+}
+
+void main() {
+  Child obj = new Child();
+  obj.printData();
+}
