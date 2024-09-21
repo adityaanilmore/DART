2631cf697d139d5070ff1c3d1d7c7872526b46f8
adityamore2004 <adityamore5343@gmail.com>
2024-09-21 02:18:25 -0700

    okboass

---
+++ Assignments/12 Number Systems/c1.dart
@@ -0,0 +1,20 @@
+import "dart:io";
+
+void main() {
+  stdout.write("Enter a number: ");
+  int num = int.parse(stdin.readLineSync()!);
+
+  int sum = 0;
+
+  for (int i = 1; i < num; i++) {
+    if (num % i == 0) {
+      sum += i;
+    }
+  }
+
+  if (sum == num) {
+    print("$num is a perfect number.");
+  } else {
+    print("$num is not a perfect number.");
+  }
+}
---
+++ Assignments/12 Number Systems/c2.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter a number: ");
+  int number = int.parse(stdin.readLineSync()!);
+  int cnt = 0;
+
+  for (int i = 1; i < number; i++) {
+    if (number % i == 0) {
+      cnt++;
+    }
+  }
+  if (cnt == 1) {
+    print("$number is a prime number.");
+  } else {
+    print("$number is not a prime number.");
+  }
+}
